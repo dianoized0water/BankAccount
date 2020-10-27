8521c641b85de3b7700b7c23b5ef36ae2fa788eb
diana_yang <dyang20@stuy.edu>
2020-10-26 23:12:16 -0400

    does this even make sense, no one knows

--- BankAccount.java
+++ BankAccount.java
@@ -5,11 +5,45 @@ public class BankAccount {
   private String password;
 
   // constructor?
-  public BankAccount(double bal, int ID, String pass){
-    balance = bal;
+  public BankAccount(int ID, String pass){
+    balance = 0.0;
     accountID = ID;
     password = pass;
   }
 
-  
+  // display data
+  public String toString(){
+    return ("Your Account ID is: "+accountID+\n+"Your Balance is: "+balance);
+  }
+
+  // access your info
+  public int getAccountID() {
+    return accountID;
+  }
+  public double getBalance(){
+    return balance;
+  }
+
+  // change password?
+  public void setPassword(String newPass){
+    password = newPass;
+  }
+
+  // deposits
+  public boolean deposit(double amount){
+    if (amount>0){
+      balance = balance + amount;
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  // withdrawl
+  public boolean withdraw(double amount){
+    if (amount>balance){
+      
+    }
+  }
 }
