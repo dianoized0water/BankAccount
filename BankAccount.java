28ef9da2d385f46d73e389cd3bc44e5db2ffcfe7
diana_yang <dyang20@stuy.edu>
2020-10-26 22:19:09 -0400

    created vars and constructor maybe?

---
+++ BankAccount.java
@@ -0,0 +1,15 @@
+public class BankAccount {
+  // vars?
+  private double balance;
+  private int accountID;
+  private String password;
+
+  // constructor?
+  public BankAccount(double bal, int ID, String pass){
+    balance = bal;
+    accountID = ID;
+    password = pass;
+  }
+
+  
+}
